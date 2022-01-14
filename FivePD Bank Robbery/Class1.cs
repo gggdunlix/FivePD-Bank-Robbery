5d6c8c107704c0c840a263e95e00e050c90c8b36
BuildTools <unconfigured@null.spigotmc.org>
2022-01-14 15:50:23 -0500

    Add project files.

---
+++ FivePD Bank Robbery/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FivePD;
+using FivePD.API;
+using FivePD.API.Utils;
+using CitizenFX.Core;
+
+namespace FivePD_Bank_Robbery
+{
+    public class Class1
+    {
+    }
+}
